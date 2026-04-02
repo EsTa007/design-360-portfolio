ac4d49fc734796fbba6cf3c266e35b0bbeab6bdd
Uchechukwu Ogbonnaya <uchechukwuogbonnaya58@gmail.com>
2026-04-02 13:20:11 +0100

    fade-in commit

--- real_portfolio.js
+++ real_portfolio.js
@@ -173,3 +173,22 @@ document.head.appendChild(fadeStyle);
 function toggleMenu() {
   document.querySelector(".nav-links").classList.toggle("active");
 }
+// SCROLL ANIMATION
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.add("active");
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
